5659b1a46e090855ed456382377d1e41c938728c
Sid Bentifraouine <sidbentifraouine@gmail.com>
2018-12-28 00:22:09 +0100

    feat(leagues): can add a league

--- src/main/java/com/sid/pokegiciel/controller/LeagueController.java
+++ src/main/java/com/sid/pokegiciel/controller/LeagueController.java
@@ -1,19 +1,31 @@
 package com.sid.pokegiciel.controller;
 
+import com.sid.pokegiciel.model.League;
+import com.sid.pokegiciel.repository.LeagueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LeagueController {
+
+    @Autowired
+    LeagueRepository leagueRepository;
+
     @RequestMapping(value = "/leagues")
-    public String getLeaguesPage() {
+    public String getLeaguesPage(Model model) {
+        model.addAttribute("leagues", leagueRepository.findAll());
         return "leagues";
     }
 
     @RequestMapping(value = "/leagues/post", method = RequestMethod.POST)
     public String postLeague(@RequestParam("name") String name) {
+        League newLeague = new League();
+        newLeague.setName(name);
+        leagueRepository.save(newLeague);
         return "redirect:/leagues";
     }
 }
